84d857ef2ec65abc14444d19abc2ce9bc9552532
shivanand9019 <shivanandmadar512@gmail.com>
2026-03-13 23:39:11 +0530

    disable datasource and jpa autoconfiguration

--- src/main/java/com/backend/url/shortener/UrlshortenerApplication.java
+++ src/main/java/com/backend/url/shortener/UrlshortenerApplication.java
@@ -2,9 +2,10 @@ package com.backend.url.shortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class UrlshortenerApplication {
 
     public static void main(String[] args) {
