73ce9c1601748ccb1674f9e59fe0ba89ab5efd8a
shivanand9019 <shivanandmadar512@gmail.com>
2026-03-13 23:54:59 +0530

    restore JPA configuration

--- src/main/java/com/backend/url/shortener/UrlshortenerApplication.java
+++ src/main/java/com/backend/url/shortener/UrlshortenerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 public class UrlshortenerApplication {
 
     public static void main(String[] args) {
